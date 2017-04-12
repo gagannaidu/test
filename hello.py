b110221662ce17bd81b0568d20494edf008d0b0c
Gagan <gagan.globin76@gmail.com>
2017-04-12 13:30:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ a = int(input("enter the first number"))
 b = int(input("enter the second number"))
 c = int(input("enter the third number"))
 
+Print ("Maximum number is:")
 print (max(a,b,c))
 
 input("Press enter button to exit")
