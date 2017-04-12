851d0bad39352259722de44601d574a5cdc96044
gagannaidu <gagan.globin76@gmail.com>
2017-04-12 13:28:00 +0530

    first python program

---
+++ hello.py
@@ -0,0 +1,7 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+c = int(input("enter the third number"))
+
+print (max(a,b,c))
+
+input("Press enter button to exit")
